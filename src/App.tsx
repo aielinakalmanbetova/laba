cae2bb61baba974fe2a2fc094a10d57e19e92959
Aielina Kalmanbetova <aielinakalmanbetova@gmail.com>
2024-05-21 01:55:55 +0600

    Level 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Card from './components/Card/Card';
 
 const App = () => {
 
   return (
-    <>
-    </>
+    <div className='playingCards faceImages'>
+      <Card rank={'K'} suit={'diams'}/>
+      <Card rank={'A'} suit={'hearts'}/>
+    </div>
   )
 };
 
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+
+interface Props {
+    rank: string;
+    suit: string;
+}
+const Card: React.FC<Props> = ({rank, suit}) => {
+
+    const getRankSymbol = (rank: string) => {
+       switch (rank) {
+           case '2':
+               return '2';
+           case '3':
+               return '3';
+           case '4':
+               return '4';
+           case '5':
+               return '5';
+           case '6':
+               return '6';
+           case '7':
+               return '7';
+           case '8':
+               return '8';
+           case '9':
+               return '9';
+           case '10':
+               return '10';
+           case 'J':
+               return 'J';
+           case 'Q':
+               return 'Q';
+           case 'K':
+               return 'K';
+           case 'A':
+               return 'A';
+           default:
+               return '';
+       }
+    };
+    const getSuitSymbol = (suit: string) => {
+        switch (suit) {
+            case 'diams':
+                return'♦';
+            case 'hearts':
+                return '♥';
+            case 'clubs':
+                return '♣';
+            case 'spades':
+                return '♠';
+            default:
+                return '';
+        }
+    };
+
+    return (
+        <span className={`card rank-${rank} ${suit}`}>
+            <span className="rank">{getRankSymbol(rank)}</span>
+            <span className="suit">{getSuitSymbol(suit)}</span>
+        </span>
+
+    );
+};
+
+export default Card;
